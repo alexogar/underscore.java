0c2ee981d4675e13dbac6183ca751bacc1ff95e5
Mikhail Vnukov <mikhail.vnukov@gmail.com>
2013-03-03 11:57:42 +0400

    Right reduce

--- src/test/java/org/underscore/ReduceTest.java
+++ src/test/java/org/underscore/ReduceTest.java
@@ -3,10 +3,29 @@ package org.underscore;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
+import static org.underscore.$.$;
+
 public class ReduceTest {
     @Test
     public void testReduce() {
-        int result = $.reduce(new Integer[]{1, 2, 3}, 1, (Integer memo, Integer j) -> memo * j);
+        long result = $.reduce(new Integer[]{1, 2, 3}, 1l, (Long memo, Integer j) -> memo * j);
         Assert.assertEquals(6, result);
     }
+
+    @Test
+    public void testReduceRight() {
+        Integer[] testArray = {1, 2, 3};
+        Integer[] expectedArray = {3, 2, 1};
+        ArrayList result = $(testArray).reduceRight(
+                new ArrayList<Integer>(),
+                (ArrayList<Integer> memo, Integer j) -> {
+                    memo.add(j);
+                    return memo;
+                });
+
+        Assert.assertArrayEquals(expectedArray, $(result).array());
+    }
+
 }
