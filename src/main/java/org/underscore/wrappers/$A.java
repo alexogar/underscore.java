6009c2a25ad173c517b502d07cd0efc626a8e97b
Mikhail Udalov <mikhail.udalov@gmail.com>
2013-04-02 01:05:58 +0400

    Arrays 'first' and 'first N' functions

--- src/main/java/org/underscore/wrappers/$A.java
+++ src/main/java/org/underscore/wrappers/$A.java
@@ -15,31 +15,103 @@ public class $A<T> {
 
     private T[] internal;
 
+    /**
+     * Constructor with 'varargs' initializer.
+     *
+     * @param initial initial values
+     */
     public $A(T... initial) {
         this.internal = initial;
     }
 
+    /**
+     * Constructor with <code>Collection</code> initializer.
+     *
+     * @param initial initial values
+     */
     public $A(Collection<T> initial) {
         this.internal = (T[]) initial.toArray();
     }
 
+    /**
+     * Flattens a nested arrays (the nesting can be to any depth), creates new array and doesn't change internal state.
+     * <p/>
+     * Example:
+     * $A(new String[]{"1"}, "2", new String[]{"3"}).flatten() => ["1", "2", "3"]
+     *
+     * @return flattened array
+     */
     public T[] flatten() {
         List<T> result = new ArrayList<>();
         $.each(this.internal, (T o) -> {
             if (o.getClass().isArray()) {
-                List<T> nested = Arrays.asList($A((T[])o).flatten());
+                List<T> nested = Arrays.asList($A((T[]) o).flatten());
                 result.addAll(nested);
             } else {
                 result.add(o);
             }
         });
-        return (T[])result.toArray();
+        return (T[]) result.toArray();
     }
 
+    /**
+     * Returns first element, or throws an exception if size of array is less than 1.
+     *
+     * @return first element
+     * @throws ArrayIndexOutOfBoundsException if size of array is less that 1
+     */
+    public T first() {
+        return internal[0];
+    }
+
+    /**
+     * Returns first 'n' elements of the array.
+     *
+     * @param n amount of elements to return
+     * @return first elements
+     */
+    public T[] first(int n) {
+        if (n > internal.length || n <= 0) {
+            throw new IllegalArgumentException("Invalid input parameter [" + n +
+                    "] for an array of size [" + internal.length + "]");
+        }
+        return Arrays.copyOfRange(internal, 0, n);
+    }
+
+    /**
+     * Returns internal array.
+     *
+     * @return internal array
+     */
     public T[] array() {
         return this.internal;
     }
 
+    /**
+     * Returns first 'n' elements of the array
+     *
+     * @param array original array
+     * @param n     amount of first elements
+     * @param <T>   elements type
+     * @return first elements
+     */
+    @IncludeInMain
+    public static <T> T[] first(T[] array, int n) {
+        return $A(array).first(n);
+    }
+
+    /**
+     * Returns first 'n' elements of the collection. Order is managed by collection's iterator.
+     *
+     * @param collection original collection
+     * @param n          amount of first elements
+     * @param <T>        elements type
+     * @return first elements
+     */
+    @IncludeInMain
+    public static <T> T[] first(Collection<T> collection, int n) {
+        return $A(collection).first(n);
+    }
 
     @IncludeInMain
     public static Object[] flatten($C c) {
@@ -49,6 +121,11 @@ public class $A<T> {
 
     @IncludeInMain
     public static <T> $A<T> $A(T... values) {
+        return new $A(values);
+    }
+
+    @IncludeInMain
+    public static <T> $A<T> $A(Collection<T> values) {
         return new $A(values);
     }
 
--- src/test/java/org/underscore/ArrayTest.java
+++ src/test/java/org/underscore/ArrayTest.java
@@ -1,14 +1,79 @@
 package org.underscore;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.underscore.$.*;
 
 public class ArrayTest {
 
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
     @Test
-    public void flattenTest() {
+    public void testFirst() {
+
+        assertEquals("1", $A("1").first());
+
+        assertEquals("1", $A("1", "2", "3").first());
+
+        exception.expect(IndexOutOfBoundsException.class);
+        $A().first();
+
+    }
+
+    @Test
+    public void testFirstN() {
+
+        assertArrayEquals($A("1").array(),
+                $A("1", "2", "3").first(1));
+
+        assertArrayEquals($A("1", "2").array(),
+                $A("1", "2", "3").first(2));
+
+        exception.expect(IllegalArgumentException.class);
+        $A("1").first(2);
+
+    }
+
+    @Test
+    public void testFirstNUsingStatic() {
+
+        assertArrayEquals(
+              $A("1", "2").array(),
+              $.first(Arrays.asList("1", "2", "3"), 2)
+        );
+
+        assertArrayEquals(
+                $A("1", "2").array(),
+                $.first(new String[]{"1", "2", "3"}, 2)
+        );
+
+    }
+
+    @Test
+    public void testFirstNFailsWithZero() {
+
+        exception.expect(IllegalArgumentException.class);
+        $A("1").first(0);
+
+    }
+
+    @Test
+    public void testFirstNFailsWithNegative() {
+
+        exception.expect(IllegalArgumentException.class);
+        $A("1").first(-1);
+
+    }
+
+    @Test
+    public void testFlatten() {
 
         assertArrayEquals(
                 $A(1, 2, 3).array(),
@@ -18,7 +83,7 @@ public class ArrayTest {
         assertArrayEquals(
                 $A(1, 2, 3, 4, 5).array(),
                 $.flatten(
-                       $A(1, $A(2, 3).array(), $A(4, 5).array()).array()
+                        $A(1, $A(2, 3).array(), $A(4, 5).array()).array()
                 ));
 
         Object[] nested = $A(1, $A(1).array()).array();
@@ -35,7 +100,12 @@ public class ArrayTest {
                         $A('1', $A('2').array(), $A('3').array()).array()
                 ));
 
+        // plain nested arrays
+        assertArrayEquals(
+                $A(new String[]{"1"}, "2", new String[]{"3"}).flatten(),
+                new String[]{"1", "2", "3"});
 
     }
+
 
 }
